d362758b576ce71309f631bcb4429bf325adcec0
Douzal <Alexis_Masson_@hotmail.fr>
2018-09-19 12:00:00 +0200

    MAJ dossier Salesforce - Ajout des DEX450, DEX460, DEX601 et maj des documents

--- SALESFORCE - FORMATION/DEV 450/DEV450_Code_Test42.java
+++ SALESFORCE - FORMATION/DEV 450/DEV450_Code_Test42.java
@@ -539,5 +539,26 @@ public class Test_42 {
         Contact con = Database.query(req);        
         return con;
     }
+
+    public static void TestSchemaDescribe () {
+        // methode issue de SF -> https://developer.salesforce.com/docs/atlas.en-us.apexcode.meta/apexcode/apex_dynamic_describeSObject.htm
+        // sObject types to describe
+        String[] types = new String[]{'Account','Contact', 'Broker__c'};
+            
+        // Make the describe call
+        Schema.DescribeSobjectResult[] results = Schema.describeSObjects(types);
+    
+        // For each returned result, get some info
+        for(Schema.DescribeSobjectResult res : results) {
+            System.debug('---sObject Label: ' + res.getLabel());
+            System.debug('---Number of fields: ' + res.fields.getMap().size());
+            System.debug('---' + res.isCustom() ? 'This is a custom object.' : 'This is a standard object.');
+            // Get child relationships
+            Schema.ChildRelationship[] rels = res.getChildRelationships();
+            if (rels.size() > 0) {
+                System.debug(res.getName() + ' has ' + rels.size() + ' child relationships.');
+            }
+        }
+    }
     
 }
